ebcf34735afdefe693817b1fc360e1508994c724
Mauricio Cartagena <mc973220@gmail.com>
2020-11-26 22:34:10 -0400

    sección 3 terminada

--- models/sockets.js
+++ models/sockets.js
@@ -16,9 +16,37 @@ class Sockets {
         this.io.on('connection', ( socket ) => {
 
             console.log('Cliente conectado');
-
+            
             //  Emitir al cliente conectado, todas las bandas actuales
             socket.emit('current-bands', this.bandList.getBands() );
+
+            // votar por la banda
+            socket.on( 'votar-banda', ( id ) => {
+                this.bandList.increaseVotes( id );
+                this.io.emit('current-bands', this.bandList.getBands() );
+            });
+
+            // Borrar banda
+            socket.on( 'borrar-banda', ( id ) => {
+
+                this.bandList.removeBand( id );
+                this.io.emit('current-bands', this.bandList.getBands() );
+
+            });
+
+            // cambiar nombre de la banda
+            socket.on( 'cambiar-nombre-banda', ( { id, name } ) => {
+                this.bandList.changeName( id, name );
+                this.io.emit('current-bands', this.bandList.getBands() );
+
+            });
+
+            socket.on( 'nueva-banda', ( { nombre } ) => {
+                this.bandList.addBand( nombre );
+                this.io.emit('current-bands', this.bandList.getBands() );
+
+            });
+            
 
        
         });
